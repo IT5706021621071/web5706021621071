e18ddd81befbc6ab6a011abc7be916198503ee8e
IT5706021621071 <5706021621071@fitm.kmutnb.ac.th>
2016-02-10 09:11:21 +0700

    Update Result.php

--- Result.php
+++ Result.php
@@ -12,23 +12,23 @@
 		$nickname = $_POST['nickname'];
 		$sex = $_POST['sex'];
 		$birth = $_POST['birth'];
-		$mail = $_POST['address'];
+		$mail = $_POST['mail'];
 		$tel = $_POST['tel'];
 		$branch = $_POST['branch'];
 
-if($age >= "15" && $age <= "100"){
-echo "<p>";
-echo "<b> ข้อมูลที่ผู้ใช้ใส่มา </b><br/>";
-echo "ชื่อ-นามสกุล : <i> $name </i><i> $surname </i><i> ($nickname) </i><br/>";
-echo "เพศ : <i> sex </i><br/>";
-echo "อายุ : <i> $age </i><br/>";
-echo "วัน/เดือน/ปีเกิด : <i> $birth </i><br/>";
-echo "E-mail : <i> $mail </i><br/>";
-echo "เบอร์โทรศัพท์ : <i> $tel </i><br/>";
-echo "สาขา : <i> $branch </i><br/>";
-}
+		if($age >= "15" && $age <= "100"){
+		echo "<p>";
+		echo "<b> ข้อมูลที่ผู้ใช้ใส่มา </b><br/>";
+		echo "ชื่อ-นามสกุล : <i> $name </i><i> $surname </i><i> ($nickname) </i><br/>";
+		echo "เพศ : <i> sex </i><br/>";
+		echo "อายุ : <i> $age </i><br/>";
+		echo "วัน/เดือน/ปีเกิด : <i> $birth </i><br/>";
+		echo "E-mail : <i> $mail </i><br/>";
+		echo "เบอร์โทรศัพท์ : <i> $tel </i><br/>";
+		echo "สาขา : <i> $branch </i><br/>";
+		}
 
-else{echo "อายุที่กรอกไม่ได้อยู่ในช่วงที่เหมาะสม กรุณากรอกข้อมูลใหม่"}
+		else{echo "อายุที่กรอกไม่ได้อยู่ในช่วงที่เหมาะสม กรุณากรอกข้อมูลใหม่";}
 ?>
 	</body>
 </center>
