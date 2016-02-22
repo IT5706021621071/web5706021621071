7e8692eee5367e4dadaff93202ef278d36cde862
IT5706021621071 <5706021621071@fitm.kmutnb.ac.th>
2016-02-22 21:44:50 +0700

    ok
    update

--- random52.php
+++ random52.php
@@ -6,14 +6,52 @@
 <body>
   <table width="800" align="center" border="1">
 <?php
-for ($r=1 ; $r <= 13 ;$r++){
-  echo "<tr>";
-  for ($c=1 ; $c <= 4 ;$c++){
-   $v[$c] = rand(1,52);
-   echo "<td><center> $v[$c] </center></td>" ;
- }
- echo " </tr> " ;
- }
+//แก้ไขครั้งที่1 ผลลัพธ์ยังไม่ได้ต้องเปลี่ยนหลักการใหม่
+  $check = 1;
+  for($i=0;$i<52;$i++){
+    $array[$i] = $i + 1;
+  }
+  do{
+    $random = rand(1,52);
+    $temp;
+    for($i=0;$i<52;$i++)
+    {
+      if($random == $array[$i])
+      {
+        $temp = $random;
+        $array[$x] = 0;
+        break;
+      }
+      elseif ($random != $array[$i])
+      {
+      }
+    }
+
+    for($r=0; $r<13; )
+    {
+      for($c=0; $c<4; )
+      {
+        $sum[$r][$c] = $temp;
+        if($c==3)
+        {
+          $c=0;
+        }
+        $c++;
+        break;
+      }
+      $r++;
+      $check = 1;
+    }
+    /*for($r=0; $r<13; $r++)
+    {
+      echo "<tr>";
+      for($c=0; $c<4; $c++)
+      {
+        echo "<td><center>$sum[$r][$c]</center></td>";
+      }
+      echo "</tr>";
+    }*/
+  }while($check == 0);
  ?>
 </table>
 </body>
